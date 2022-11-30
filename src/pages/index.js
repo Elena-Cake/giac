e96dbaef06fa0e05a22c1e20709f1418f6949310
Elena Sveshnikova <lenamarinkevich@mail.ru>
2022-11-30 13:08:37 +0300

    webpack

---
+++ src/pages/index.js
@@ -0,0 +1,24 @@
+import './index.css';
+import {
+    inputAssingmentNum,
+    inputAssingmentDate,
+
+    inputApproachTypeOrbit,
+    inputApproachInternationalNumKA, inputApproachNoradNumKA, inputApproachGiacNumKA,
+    inputApproachInternationalNumKO, inputApproachNoradNumKO, inputApproachGiacNumKO,
+    buttonAddApproach,
+
+    buttonAddCondition,
+    buttonAddDestroy,
+    buttonAddDeorbit,
+
+    inputSpacecraftNameKA,      inputSpacecraftInternationalNumKA,
+    inputSpacecraftNoradNumKA,  inputSpacecraftGiacNumKA,
+    buttonAddSpacecraft,
+
+    inputMassage
+
+} from '../utils/constans.js'
+
+
+console.log(inputMassage)
---
+++ src/utils/constans.js
@@ -0,0 +1,36 @@
+
+// buttons
+export const buttonAddApproach = document.querySelector('.close__btn-add');
+export const buttonAddCondition = document.querySelector('.condition__btn-add');
+export const buttonAddDestroy = document.querySelector('.destroy__btn-add');
+export const buttonAddDeorbit = document.querySelector('.deorbit__btn-add');
+export const buttonAddSpacecraft = document.querySelector('.spacecraft__btn-add');
+
+export const buttonAddDocument = document.querySelector('.form__btn-save');
+export const buttonResetForm = document.querySelector('.form__btn-reset');
+
+// inputs
+
+// assingment
+export const inputAssingmentNum = document.querySelector('.assingment__input-num');
+export const inputAssingmentDate = document.querySelector('.assingment__input-date');
+
+// approach
+export const inputApproachTypeOrbit = document.querySelector('.approach__type-orbit');
+// KA
+export const inputApproachInternationalNumKA =  document.querySelector('.approach_ka_inter');
+export const inputApproachNoradNumKA = document.querySelector('.approach_ka_norad');
+export const inputApproachGiacNumKA = document.querySelector('.approach_ka_giac');
+// KO
+export const inputApproachInternationalNumKO =  document.querySelector('.approach_ko_inter');
+export const inputApproachNoradNumKO = document.querySelector('.approach_ko_norad');
+export const inputApproachGiacNumKO = document.querySelector('.approach_ko_giac');
+
+// spacecraft
+export const inputSpacecraftNameKA =  document.querySelector('.spacecraft_ka_name');
+export const inputSpacecraftInternationalNumKA =  document.querySelector('.spacecraft_ka_inter');
+export const inputSpacecraftNoradNumKA = document.querySelector('.spacecraft_ka_norad');
+export const inputSpacecraftGiacNumKA = document.querySelector('.spacecraft_ka_giac');
+
+// massage
+export const inputMassage = document.querySelector('.massage__text');
