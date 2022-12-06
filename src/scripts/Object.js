510eb911c0ea8eaa49930aa49825f9d0767ca9e4
Elena Sveshnikova <lenamarinkevich@mail.ru>
2022-12-06 12:14:23 +0300

    work btn

--- src/scripts/Object.js
+++ src/scripts/Object.js
@@ -15,14 +15,12 @@ export default class Object{
   
     createCard() {
       this._element = this._getTemplate();
-      this._interContainer = this._element.querySelector('.element_ko_inter')
-      this._noradContainer = this._element.querySelector('.element_ko_norad')
-      this._giacContainer = this._element.querySelector('.element_ko_giac')
-      this._cardTrash = this._element.querySelector('.element__btn-trash')
-
-      // this._interContainer.textContent = this._interNum;
-      // this._noradContainer.textContent = this._noradNum;
-      // this._giacContainer.textContent = this._giacNum;
+      this._listInputs = this._element.querySelectorAll('.item__input')
+      // this._interContainer = this._element.querySelector('.element_ko_inter')
+      // this._noradContainer = this._element.querySelector('.element_ko_norad')
+      // this._giacContainer = this._element.querySelector('.element_ko_giac')
+      this._buttonTrash = this._element.querySelector('.element__btn-trash')
+      this._buttonSave = this._element.querySelector('.element__btn-done')
 
       this._setEventListeners();
       return this._element;
@@ -39,13 +37,25 @@ export default class Object{
     }
   
     _setEventListeners() {
-      this._cardTrash.addEventListener ('click', this._remove);
+      this._buttonTrash.addEventListener ('click', this._remove);
+      this._buttonSave.addEventListener ('click', this._save);
       }
   
+    _save = () => {
+       this._listInputs.forEach(input =>{
+        if (input.readOnly) {
+          input.readOnly = false
 
-    _openPopupRemove = () => {
-      this._popupDelete.open(this._idCard, this._element)
+        } else {
+          input.readOnly = true
+          this._buttonSave.classList.remove('.element__btn-done')
+          this._buttonSave.classList.add('.element__btn-done_type_edit')
+       } 
+      })
     }
+    // _openPopupRemove = () => {
+    //   this._popupDelete.open(this._idCard, this._element)
+    // }
 
      _remove = () => {
       this._element.remove()
