a507d3f301c15d70367f312f3bcd1c801c852d12
Elena Sveshnikova <lenamarinkevich@mail.ru>
2022-12-19 16:18:48 +0300

    jquery install

--- src/scripts/dataTble.js
+++ src/scripts/dataTble.js
@@ -1,9 +1,5 @@
-// window.$ = window.jquery = require('../../node_modules/jquery');
-// window.dt = require('../../node_modules/datatables.net');
-// window.$('.finder__list').DataTable();
-// var $  = require( 'jquery' );
-// var dt = require( 'datatables.net' )();
 
-// $(document).ready(function () {
-//     $('.finder__list').DataTable();
-// });
+
+$(document).ready(function () {
+    $('.finder__list').DataTable();
+});
