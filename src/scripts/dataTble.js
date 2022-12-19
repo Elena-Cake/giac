2f8fa73ecf43afbf4a86b61f8c6cafe9827d3082
Elena Sveshnikova <lenamarinkevich@mail.ru>
2022-12-19 16:03:05 +0300

    not work

--- src/scripts/dataTble.js
+++ src/scripts/dataTble.js
@@ -4,21 +4,6 @@ window.$('.finder__list').DataTable();
 // var $  = require( 'jquery' );
 // var dt = require( 'datatables.net' )();
 
-export let table = new DataTable('.finder__list', {
-    data: data,
-    columns: [
-        { num: 'name' },
-        { date: 'position' },
-        { approach: 'name' },
-        { condition: 'position' },
-        { approach: 'salary' },
-        { destroy: 'name' },
-        { deorbit: 'position' },
-        { spacecraft: 'salary' },
-        { massage: 'salary' }
-    ]
-});
-
 // $(document).ready(function () {
 //     $('.finder__list').DataTable();
 // });
